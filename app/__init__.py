a5e801b60df9bc010a2b7dcb9e4c86d67c847076
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-01 13:56:44 +0300

    Added flask-login manager to help provide session management for the application

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.strong'
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
