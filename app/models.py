cdf79f921a5c121c5b88e9fd4294769f4f5aab48
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-01 13:04:17 +0300

    updated class  attributes in the database model classes.

--- app/models.py
+++ app/models.py
@@ -7,24 +7,26 @@ from datetime import datetime
 from flask_login import UserMixin
 
 
-
-
 class Admin(UserMixin,db.Model):
     '''
     class for users
     '''
     __tablename__='admins'
     id = db.Column(db.Integer,primary_key=True)
-    profile_image = db.Column(db.String, nullable = False, default = 'default_profile_image.png')
     email = db.Column(db.String(100), unique = True, index =True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
     username = db.Column(db.String(72), unique=True,index=True)
     password_hash = db.Column(db.String(255))
 
-    # Relationship with the Blog Post
+    # Relationship with the Events Post
     events = db.relationship('Event', backref = 'author', lazy = "dynamic")
 
-    def __init__(self,email, username, password):
+    def __init__(self,email, username, first_name,last_name,password):
+
         self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
         self.username =  username
         self.password_hash = generate_password_hash(password)
 
@@ -32,7 +34,7 @@ class Admin(UserMixin,db.Model):
         return check_password_hash(self.password_hash,password)
         
     def __repr__(self):
-        return f"User {self.username}"
+        return f"Admin {self.first_name}"
 
 
 
@@ -41,18 +43,16 @@ class Event(db.Model):
     '''
     Class for events
     '''
-    # Relationship with the question table
-    users = db.relationship(Question)
-
+    # Relationship with the admin table
+    admin = db.relationship(Admin)
     id = db.Column(db.Integer, primary_key = True)
-    # Relationship with the admin table 
-    admin_id = db.Column(db.Integer,db.ForeignKey('admins.id'), nullable = False)
     timestamp = db.Column(db.DateTime, index=True, nullable = False, default=datetime.utcnow)
     title = db.Column(db.String(120), nullable=False)
     body = db.Column(db.Text, nullable = False)
     event_date = db.Column(db.DateTime, index=True, nullable = False)
-    # Relationship with the questions for the event
-    admin = db.relationship('Admin', backref = 'events', lazy = "dynamic")
+
+    # Relationship with the question table 
+    question = db.relationship('Question',backref = 'events', nullable = False)
     
     def __init__(self,timestamp, title, body, event_date,user_id):
         self.title = title
@@ -63,8 +63,6 @@ class Event(db.Model):
 
     def generate_events_id():
         pass
-
-
         
     def __repr__(self):
         return f"EVENT ID:{self.id} -- Date: {self.timestamp}"    
@@ -76,94 +74,61 @@ class Question(db.Model):
     Class for Questions
     '''
     # Relationship with the events table
-    users = db.relationship(User)
-
+    events = db.relationship(Event)
     id = db.Column(db.Integer, primary_key = True)
-    # Relationship with the admin table 
-    admin_id = db.Column(db.Integer,db.ForeignKey('admins.id'), nullable = False)
-    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.String(120), nullable=False)
     # This is auto-generated and links the event to future user sessions.
     event_id = db.Column(db.String(8), nullable = False)
-    
     # Relationship with the questions for the event
-    questins = db.relationship('Question', backref = 'event', lazy = "dynamic")
+    responses = db.relationship('Answer', backref = 'questions', lazy = "dynamic")
     
-    def __init__(self,title,user_id, event_id):
-        self.admin_id = admin_id
-        self.title =title
-        self.user_id = user_id
+    def __init__(self,body, event_id):
+        self.body =body
         self.event_id = event_id
-
-
-        
+    
     def __repr__(self):
-        return f"Question:{self.title} -- Admin: {self.admin_id}--Event:{self.event_id}"    
-
-
+        return f"Question:{self.body} -- Event: {self.event_id}"    
 
 class Response(db.Model):
     __tablename__ = "responses"
     '''
     Class for Responses
     '''
-    # Relationship with the events table
-    users = db.relationship(User)
+    # Relationship with the questions table
+    questions = db.relationship(Question)
 
     id = db.Column(db.Integer, primary_key = True)
-    # Relationship with the admin table 
-    question_id = db.Column(db.Integer,db.ForeignKey('admins.id'), nullable = False)
     timestamp = db.Column(db.DateTime, index=True, nullable = False, default=datetime.utcnow)
     body = db.Column(db.Text, nullable = False)
-    # This is auto-generated and links the event to future user sessions.
-    event_id = db.Column(db.String(8), nullable = False)
-    
     # Relationship with the questions for the event
-    questins = db.relationship('Question', backref = 'event', lazy = "dynamic")
+    users = db.relationship('User', backref = 'questions', lazy = "dynamic")
     
-    def __init__(self,title, body, user_id):
-        self.title = title
+    def __init__(self,question,timestamp,title, body, users):
+        self.question = question
+        self.timestamp = timestamp
         self.body = body
-        self.user_id = user_id
-
-    def generate_events_id():
-        pass
-
-
-        
+        self.users = users
+   
     def __repr__(self):
-        return f"EVENT ID:{self.id} -- Date: {self.timestamp}"    
+        return f"Question :{self.question} -- Response: {self.body}"    
 
 
-
-
-
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = "users"
     '''
     Class for users
     '''
-    # Relationship with the events table
-    questions = db.relationship(Question)
-
+    # Relationship with the Responses table
+    responses = db.relationship(Response)
     id = db.Column(db.Integer, primary_key = True)
-    # Relationship with the admin table 
-    question_id = db.Column(db.Integer,db.ForeignKey('admins.id'), nullable = False)
     timestamp = db.Column(db.DateTime, index=True, nullable = False, default=datetime.utcnow)
     device_id = db.Column(db.Text, nullable = False)
 
-    
-    # Relationship with the questions for the event
-    questins = db.relationship('Question', backref = 'event', lazy = "dynamic")
-    
     def __init__(self,question_id, device_id,body, user_id):
         self.question_id = question_id
         self.device_id - device_id
         self.user_id = user_id
 
-    def generate_device_id():
-        pass
-
-
         
     def __repr__(self):
-        return f"USER ID:{self.device_id}"    
+        return f"USER ID:{self.device_id}----Respons ID:{self.question_id}"    
