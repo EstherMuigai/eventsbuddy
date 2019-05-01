ab6e3ba4890f089af75edcacc9b5b23330cf69c3
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-01 13:06:38 +0300

    Uncommented database configurations/extensins that were initially disabled.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-##from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 bootstrap = Bootstrap()
-##db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
 
 # Initializing flask extensions
     bootstrap.init_app(app)
-    ##db.init_app(app)
+    db.init_app(app)
 
 # Creating the app configurations
     app.config.from_object(config_options[config_name])
