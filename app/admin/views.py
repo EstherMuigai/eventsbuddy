4f65de04a0ee0a9f712c397d8680526a2ef207df
maina murage <mainamurage21@gmail.com>
2019-05-02 17:59:37 +0300

    Add routing and template navigation for admin&auth

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,5 +2,21 @@ from flask import render_template
 from . import admin
 
 @admin.route('/')
-def test_admin():
-    return render_template('admin/test_admin.html')
+def admin_welcome():
+    return render_template('admin/welcome.html')
+
+@admin.route('/dashboard')    
+def dashboard():
+    return render_template('admin/dashboard.html')
+
+@admin.route('/create_event')    
+def create_event():
+    return render_template('admin/create_event.html')
+
+@admin.route('/event_page')    
+def event_page():
+    return render_template('admin/event_page.html')
+
+@admin.route('/create_question')    
+def create_question():
+    return render_template('admin/create_question.html')
